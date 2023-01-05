ff293274d57a0dc2a7f393a617a7bc4a1ab63d8c
Edward Chen <edward9065@gmail.com>
2023-01-04 20:50:35 -0500

    first attempt at creating sudoku grid represenation and added test classes

--- src/main/java/org/example/Cell.java
+++ src/main/java/org/example/Cell.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class Cell {
     SudokuContainer row, column, box, possibilities;
-    int value;
+    private int value;
     public Cell (SudokuContainer row, SudokuContainer column, SudokuContainer box, int value) {
         this.row = row;
         this.column = column;
         this.box = box;
         if(value > 9 || value < 0) {
-            throw new IllegalArgumentException("Error value of cell must be a value between 0 and 9")
+            throw new IllegalArgumentException("Error value of cell must be a value between 0 and 9");
         }
         this.value = value;
     }
@@ -21,8 +21,19 @@ public class Cell {
         this.value = value;
     }
 
+    //throw exception later, return 0 now
     public int getNextPossibleValue() {
         //do smth
+        for(int i = 1; i<9; i++) {
+            if(!possibilities.isNumberPresent(i)) {
+                if(row.isNumberPresent(i) || column.isNumberPresent(i) || box.isNumberPresent(i)) {
+                    possibilities.addNumber(i);
+                } else {
+                    return i;
+                }
+            }
+        }
+        return 0;
     }
 
     public SudokuContainer getRow() {
--- src/main/java/org/example/SudokuGridImpl.java
+++ src/main/java/org/example/SudokuGridImpl.java
@@ -1,32 +1,61 @@
 package org.example;
 
 public class SudokuGridImpl implements SudokuGrid{
-    int[][] grid;
+    Cell[][] grid;
     SudokuContainer[] rows, columns, boxes;
 
-    public SudokuGridImpl(){
-        grid = new int[9][9];
+    public SudokuGridImpl(int[][] inputGrid){
+        if(inputGrid.length !=9) {
+            throw new IllegalArgumentException("Error, input grid must be 9x9");
+        }
+
+        grid = new Cell[9][9];
         rows = new SudokuContainer[9];
         columns = new SudokuContainer[9];
         boxes = new SudokuContainer[9];
+
+        int boxCount = 0;
+
+        for(int i=0; i<9; i++) {
+            if(inputGrid[i].length!=0) { throw new IllegalArgumentException("Error, input grid must be 9x9"); }
+            //create new row
+            rows[i] = new SudokuContainer();
+            for(int j = 0; j<9; j++) {
+                if((i==0 || i==3 || i==6) && (j==0 || j==3 || j==6)) {
+                    //create new column and box
+                    columns[j] = new SudokuContainer();
+                    //probably could just be a list but oh well...
+                    boxes[boxCount] = new SudokuContainer();
+                } else if(j==0) {
+                    //create new column
+                    columns[j] = new SudokuContainer();
+                }
+
+                //refactor so that rows, columns, etc are different classes so that the compiler can enforce that a cell will get one of each
+                grid[i][j] = new Cell(rows[i], columns[j], boxes[boxCount], inputGrid[i][j]);
+            }
+        }
+
         //instantiate new cells
     }
     //add one that takes a JSON later
 
     //will return 0 for a non-value
     public int getCellValue(int row, int column) {
-        return grid[0][0];
+        return grid[0][0].getValue();
     }
 
     //sets the cell value regardless
     public void setCellValue(int row, int column, int value) {
-        if(rows[row].isNumberPresent(value) || columns[column].isNumberPresent(value) || ) {
-
-        }
+        grid[row][column].setValue(value);
     }
 
-    //for backtracking algprithm
+    //for backtracking algorithm
     public void setCellValueToNextValidValue(int row, int column) {
-
+        int nextValue = grid[row][column].getNextPossibleValue();
+        if(nextValue == 0) {
+            throw new IllegalStateException("Error: Cell at " + row +", "+column+" does not have a possible value");
+        }
+        grid[row][column].setValue(nextValue);
     }
 }
