677765d81865d4cf833283bad9b56eb245b487e8
Edward Chen <edward9065@gmail.com>
2023-01-04 20:57:47 -0500

    extract fillInGrid method from constructor

--- src/main/java/org/example/SudokuGridImpl.java
+++ src/main/java/org/example/SudokuGridImpl.java
@@ -5,38 +5,12 @@ public class SudokuGridImpl implements SudokuGrid{
     SudokuContainer[] rows, columns, boxes;
 
     public SudokuGridImpl(int[][] inputGrid){
-        if(inputGrid.length !=9) {
-            throw new IllegalArgumentException("Error, input grid must be 9x9");
-        }
-
         grid = new Cell[9][9];
         rows = new SudokuContainer[9];
         columns = new SudokuContainer[9];
         boxes = new SudokuContainer[9];
 
-        int boxCount = 0;
-
-        for(int i=0; i<9; i++) {
-            if(inputGrid[i].length!=0) { throw new IllegalArgumentException("Error, input grid must be 9x9"); }
-            //create new row
-            rows[i] = new SudokuContainer();
-            for(int j = 0; j<9; j++) {
-                if((i==0 || i==3 || i==6) && (j==0 || j==3 || j==6)) {
-                    //create new column and box
-                    columns[j] = new SudokuContainer();
-                    //probably could just be a list but oh well...
-                    boxes[boxCount] = new SudokuContainer();
-                } else if(j==0) {
-                    //create new column
-                    columns[j] = new SudokuContainer();
-                }
-
-                //refactor so that rows, columns, etc are different classes so that the compiler can enforce that a cell will get one of each
-                grid[i][j] = new Cell(rows[i], columns[j], boxes[boxCount], inputGrid[i][j]);
-            }
-        }
-
-        //instantiate new cells
+        fillInGrid(inputGrid);
     }
     //add one that takes a JSON later
 
@@ -57,5 +31,30 @@ public class SudokuGridImpl implements SudokuGrid{
             throw new IllegalStateException("Error: Cell at " + row +", "+column+" does not have a possible value");
         }
         grid[row][column].setValue(nextValue);
+    }
+
+    private void fillInGrid(int[][] inputGrid){
+        if(inputGrid.length !=9) { throw new IllegalArgumentException("Error, input grid must be 9x9"); }
+
+        int boxCount = 0;
+
+        for(int i=0; i<9; i++) {
+            if(inputGrid[i].length!=0) { throw new IllegalArgumentException("Error, input grid must be 9x9"); }
+            //create new row
+            rows[i] = new SudokuContainer();
+            for(int j = 0; j<9; j++) {
+                if((i==0 || i==3 || i==6) && (j==0 || j==3 || j==6)) {
+                    //create new column and box
+                    columns[j] = new SudokuContainer();
+                    //probably could just be a list but oh well...
+                    boxes[boxCount] = new SudokuContainer();
+                } else if(j==0) {
+                    //create new column
+                    columns[j] = new SudokuContainer();
+                }
+                //refactor so that rows, columns, etc are different classes so that the compiler can enforce that a cell will get one of each
+                grid[i][j] = new Cell(rows[i], columns[j], boxes[boxCount], inputGrid[i][j]);
+            }
+        }
     }
 }
